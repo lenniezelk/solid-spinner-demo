783310e0c616f3139bb806fd8f62d55ad9d2bc36
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2022-12-10 00:07:16 +0300

    initial commit

---
+++ src/root.tsx
@@ -0,0 +1,39 @@
+// @refresh reload
+import { Suspense } from 'solid-js';
+import {
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+} from 'solid-start';
+import './root.css';
+
+export default function Root() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta
+          name="description"
+          content="Dependency free SVG Spinners for SolidJS"
+        />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Title } from 'solid-start';
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Solid Spinner | Home</Title>
+      <h1>Hello world!</h1>
+    </main>
+  );
+}
